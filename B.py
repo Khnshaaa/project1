4d4207067d902349befe7a57727872343aa60378
Khnshaaa <k_kaldybay@kbtu.kz>
2025-01-15 23:41:16 +0500

    a

---
+++ B.py
@@ -0,0 +1,26 @@
+#1
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+
+#2
+x = 5
+y = 3
+
+print(x + y)  
+print(x == y) 
+print(x > y and y < 10)  
+
+#3
+age = 18
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
---
+++ d.py
@@ -0,0 +1,18 @@
+s = "hello"
+s2 = "World"
+result = s + " " + s2
+print(result)
+
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+a = 1
+b = 35656222554887711
+c = -3255522
+
+print(type(a))
+print(type(b))
+print(type(c))

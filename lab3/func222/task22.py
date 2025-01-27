2cfb424bf809a7136ec62fd1667bf82abae73228
Khnshaaa <k_kaldybay@kbtu.kz>
2025-01-28 00:50:53 +0500

    a

--- lab3/func222/task22.py
+++ lab3/func222/task22.py
@@ -3,7 +3,6 @@ from movies_data import movies
 
 def get_highly_rated_movies(movies):
     return [movie for movie in movies if movie["imdb"] > 5.5]
-\
 highly_rated_movies = get_highly_rated_movies(movies)
 
 # Printing the result

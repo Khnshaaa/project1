d046f713d01748ba89087b7a1ef379545ab3db2d
khanshaiym <hanshaim0901@gmail.com>
2025-02-02 16:47:35 +0500

    5 task

--- lab3/functions1111/a14.py
+++ lab3/functions1111/a14.py
@@ -2,15 +2,10 @@
 # some of the functions from the above 13 tasks 
 # and try to use them.
 from functions1111 import is_polinderome
-
-
 words=['apple','banana','alaala','data','elderberry','apple','banana']
 for i in words:
     if is_polinderome(i):
         print(i)
     else:
         print('not a palindrome')   
-
-
-#hddcbcn.djmljdflkdf
     

093c2d1c91ca78cf0a575e6a537f25e9d03bedb0
khanshaiym <hanshaim0901@gmail.com>
2025-02-12 10:36:46 +0500

    ы

--- lab4/generators/c2.py
+++ lab4/generators/c2.py
@@ -9,9 +9,6 @@ def even_numbers(n):
     for num in range(0, n+1, 2):  
         even_nums.append(num)  
     return even_nums 
-
 n = int(input())
 even_nums_list = even_numbers(n)
-
-# Print the even numbers in comma-separated form
 print(", ".join(map(str, even_nums_list)))
